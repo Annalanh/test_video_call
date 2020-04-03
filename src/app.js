9e85bbf2c58072020d12f03bc0dddc3d57ada947
lanhphuongthao@yahoo.com <thaonp@fimo.edu.vn>
2020-04-03 16:16:35 +0700

    bo bot stun

--- src/app.js
+++ src/app.js
@@ -13,12 +13,12 @@ openStream(function (stream) {
                 {
                     urls: "stun:ss-turn2.xirsys.com"
                 },
-                { urls: 'stun:stun.l.google.com:19302' },
-                { urls: 'stun:stun1.l.google.com:19302' },
-                { urls: 'stun:stun2.l.google.com:19302' },
-                { urls: 'stun:stun3.l.google.com:19302' },
-                { urls: 'stun:stun4.l.google.com:19302' },
-                { urls: 'stun:global.stun.twilio.com:3478?transport=udp' },
+                // { urls: 'stun:stun.l.google.com:19302' },
+                // { urls: 'stun:stun1.l.google.com:19302' },
+                // { urls: 'stun:stun2.l.google.com:19302' },
+                // { urls: 'stun:stun3.l.google.com:19302' },
+                // { urls: 'stun:stun4.l.google.com:19302' },
+                // { urls: 'stun:global.stun.twilio.com:3478?transport=udp' },
                 // {
                 //     username: "k3sjhrG8bf_JtVATzpMHsd2TAWTuBIxDbJXchEdP7e_4LYMCen7TlZJkxE2PRjrCAAAAAF6G4D90aGFvZ3Vt",
                 //     urls: "turn:ss-turn2.xirsys.com:80?transport=udp",
@@ -29,16 +29,16 @@ openStream(function (stream) {
                 //     urls: "turn:ss-turn2.xirsys.com:3478?transport=udp",
                 //     credential: "841f7188-7579-11ea-8dfd-12fa47b8f761"
                 // },
-                {
-                    username: "k3sjhrG8bf_JtVATzpMHsd2TAWTuBIxDbJXchEdP7e_4LYMCen7TlZJkxE2PRjrCAAAAAF6G4D90aGFvZ3Vt",
-                    urls: "turn:ss-turn2.xirsys.com:80?transport=tcp",
-                    credential: "841f7188-7579-11ea-8dfd-12fa47b8f761"
-                },
-                {
-                    username: "k3sjhrG8bf_JtVATzpMHsd2TAWTuBIxDbJXchEdP7e_4LYMCen7TlZJkxE2PRjrCAAAAAF6G4D90aGFvZ3Vt",
-                    urls: "turn:ss-turn2.xirsys.com:3478?transport=tcp",
-                    credential: "841f7188-7579-11ea-8dfd-12fa47b8f761"
-                },
+                // {
+                //     username: "k3sjhrG8bf_JtVATzpMHsd2TAWTuBIxDbJXchEdP7e_4LYMCen7TlZJkxE2PRjrCAAAAAF6G4D90aGFvZ3Vt",
+                //     urls: "turn:ss-turn2.xirsys.com:80?transport=tcp",
+                //     credential: "841f7188-7579-11ea-8dfd-12fa47b8f761"
+                // },
+                // {
+                //     username: "k3sjhrG8bf_JtVATzpMHsd2TAWTuBIxDbJXchEdP7e_4LYMCen7TlZJkxE2PRjrCAAAAAF6G4D90aGFvZ3Vt",
+                //     urls: "turn:ss-turn2.xirsys.com:3478?transport=tcp",
+                //     credential: "841f7188-7579-11ea-8dfd-12fa47b8f761"
+                // },
                 {
                     username: "k3sjhrG8bf_JtVATzpMHsd2TAWTuBIxDbJXchEdP7e_4LYMCen7TlZJkxE2PRjrCAAAAAF6G4D90aGFvZ3Vt",
                     urls: "turns:ss-turn2.xirsys.com:443?transport=tcp",
